f6a46f7aad8525ea32c6cef337689dcf85d46e71
mydeo91 <leh2030@icloud.com>
2019-04-10 13:54:30 +0900

    #3-1 Installing Redux in Our Project

---
+++ src/redux/users.js
@@ -0,0 +1,26 @@
+// import
+
+// actions
+
+// action creator
+
+// init state
+const initialState = {
+  isLoggedIn: localStorage.getItem("jwt") || false
+};
+
+// reducer
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+// reducer functions
+
+// exports
+
+// reducer export
+
+export default reducer;
