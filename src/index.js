aab4553b008ea525115b81829e23d6df3465d86c
mydeo91 <leh2030@icloud.com>
2019-04-10 14:21:14 +0900

    #3-3 Connecting the Reducer with the React App

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// console.log(store.getState()); // Check state of reducers in store
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+, document.getElementById('root'));
