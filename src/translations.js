766b6ec097b28d014c5f2600e7649dc72569f13c
mydeo91 <leh2030@icloud.com>
2019-04-11 20:33:35 +0900

    #3-32 Importing Translations

--- src/translations.js
+++ src/translations.js
@@ -1,25 +1,21 @@
 export const translations = {
-    ko: {
-        "About us": "Instagram 정보",
-        "Support": "지원",
-        "Press": "홍보 센터",
-        "API": "",
-        "Jobs": "채용 정보",
-        "Privacy": "개인정보처리방침",
-        "Terms": "약관",
-        "Directory": "디렉터리",
-        "Language": "언어",
-
-        "Username": "사용자 이름",
-        "Password": "비밀번호",
-        "Log in": "로그인",
-        "Sign in": "가입하기",
-        "or": "또는",
-        "Log in with Facebook": "Facebook으로 로그인",
-        "Forgot password?": "비밀번호를 잊으셨나요?",
-        "Don't have account?": "계정이 없으신가요?",
-        "Have an account?": "계정이 있으신가요?",
-        "Get the app": "앱을 다운로드하세요",
-    }
-};
-
+  'ko': {
+    'Username': '사용자 이름',
+    'Password': '비밀번호',
+    'Log in': '로그인',
+    'or': '또는',
+    'Log in with Facebook': 'Facebook으로 로그인 하기',
+    'Forgot password?': '비밀번호를 잊으셨나요?',
+    'About us': 'Instagram 정보',
+    'Support': '지원',
+    'Press': '홍보센터',
+    'API': '',
+    'Jobs': '채용정보',
+    'Privacy': '개인정보처리방침',
+    'Terms': '약관',
+    'Directory': '디렉터리',
+    'Language': '언어',
+  },
+  'options': {
+  },
+}
